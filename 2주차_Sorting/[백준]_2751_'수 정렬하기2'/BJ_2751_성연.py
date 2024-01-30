496c9d9456e89a5f849883f3f42e4ff8c20e1d66
tjddus <rlatjddus5767@naver.com>
2024-01-31 00:44:18 +0900

    [백준]_2751 _C_X

---
+++ 2주차_Sorting/[백준]_2751_'수 정렬하기2'/BJ_2751_성연.py
@@ -0,0 +1,19 @@
+# 백준 2751번
+# 2750번과 시간, 메모리, N의 범위를 제외하면 동일한 문제인 것 같아 코드를 복붙 해보았는데 시간초과가 났다
+n = int(input())  # 첫 번째 정수 입력받기
+nums = [int(input()) for _ in range(n)]  # n번만큼 정수 입력받기, 문제에서 숫자 중복은 안된다고 나온다
+
+
+def bubbleSort(nums):
+    numlen = len(nums) # nums의 길이
+    i = 0
+    for _ in range(numlen-1):  # for문 수행한 결과를 모든 요소마다 실행해야 하므로 다시 for문 사용
+        for i in range(numlen-1): # 아래 과정을 길이만큼 반복
+                if nums[i] > nums[i+1]: # nums[i]가 nums[i+1]보다 크다면 서로 위치를 바꾼다
+                    nums[i], nums[i+1] = nums[i+1], nums[i]
+    return nums
+
+bubbleSort(nums)
+
+for i in nums:
+    print(i)
