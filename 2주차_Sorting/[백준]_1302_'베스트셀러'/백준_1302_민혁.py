3f02cd188d1151ae585f588d52c7d06a4ddc610d
민혁 <Hyeok8920@gmail.com>
2024-01-31 11:27:58 +0900

    백준_1302_B_O

---
+++ 2주차_Sorting/[백준]_1302_'베스트셀러'/백준_1302_민혁.py
@@ -0,0 +1,16 @@
+# strip() 메서드는 책 제목의 공백을 제거하여 같은 제목임을 구분하기 위해 사용(도움)
+
+from collections import Counter
+
+def best_seller(books):
+    counts = Counter(books)
+    max_count = max(counts.values())
+    # 가장 많이 팔린 책을 items()에 넣어 베스트셀러를 선정
+    best_sellers = [book for book, count in counts.items() if count == max_count]
+    return sorted(best_sellers)[0] # 가장 첫번째 정렬되어 있는 리스트를 반환
+
+if __name__ == "__main__":
+    N = int(input())
+    # strip() 메서드 사용하여 공백제거
+    books = [input().strip() for _ in range(N)]
+    print(best_seller(books))
